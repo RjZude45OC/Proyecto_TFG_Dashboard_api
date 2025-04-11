15c2703af56062b85297a78464e926502e2ab80d
RjZude45OC <152610062+RjZude45OC@users.noreply.github.com>
2025-04-11 10:55:43 +0200

    Add files via upload

---
+++ api/src/main/java/com/tfg/dashboard/api/ApiApplication.java
@@ -0,0 +1,13 @@
+package com.tfg.dashboard.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.tfg.dashboard")
+public class ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiApplication.class, args);
+	}
+
+}
