29a4b33b6f447e624611c749f0bf6d39b2063581
RjZude45OC <zhaobowen622@gmail.com>
2025-04-23 15:28:53 +0200

    changed api endpoint for HelloController

--- src/main/java/com/tfg/dashboard/controller/HelloController.java
+++ src/main/java/com/tfg/dashboard/controller/HelloController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @GetMapping("/")
+    @GetMapping("/health")
     public String helloworld(){
-        return "hello world";
+        return "healthy";
     }
 
 }
